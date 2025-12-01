f151f68627fa03e89efeaf90857129b475d5d1f3
AdityaVermaeng <adityafriendrahul@gmail.com>
2025-12-01 16:11:51 +0530

    cpp basics

---
+++ conditional/if-else.cpp
@@ -0,0 +1,15 @@
+//if-else
+#include<iostream>
+using namespace std;
+int main(){
+  int age=23;
+  if(age>=18){
+    cout<<"Eligible to vote" ;
+  }
+  else{
+    cout<<"not eligible to vote";
+  }
+return 0;
+}
+
+
